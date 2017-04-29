ce957c405b6c0d0e29a0ccdf022f990f819cdc6a
Raphael Almeida <raph.tads2013@gmail.com>
2017-04-29 10:10:04 -0300

    DataSet atualização, tableName Funcionario atualização

--- src/com/farmacontrol/usuario/Funcionario.java
+++ src/com/farmacontrol/usuario/Funcionario.java
@@ -2,7 +2,7 @@ package com.farmacontrol.usuario;
 import javax.persistence.*;
 
 @Entity
-@Table(name="tb_cliente")
+@Table(name="tb_funcionario")
 @DiscriminatorValue(value="F")
 @PrimaryKeyJoinColumn(name="id_usuario", referencedColumnName="id")
 public class Funcionario extends Usuario{
